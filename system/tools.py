96553409336b2067147de66eaa0812fa624e76d7
epitaxy <31850335+epitaxy@users.noreply.github.com>
2022-09-20 09:07:43 +0900

    Create tools.py

---
+++ system/tools.py
@@ -0,0 +1,9 @@
+import platform # OS check
+
+def OS_check() -> str:
+    release = platform.release()
+    version = platform.version()
+    os_type = platform.system()
+    platform = platform.platform()
+    print (platform, os_type, version, release)
+    return {"platform": platform, "os_type": os_type, "version": version, "release": release}
