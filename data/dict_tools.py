7fdec22f4a68ccc02f3d81fc078c50c731c98022
epitaxy <31850335+epitaxy@users.noreply.github.com>
2022-09-21 11:17:42 +0900

    Update dict_tools.py

--- data/dict_tools.py
+++ data/dict_tools.py
@@ -1,7 +1,9 @@
-def update_dict(input: dict, key: str, value) -> dict:
+def update_dict(input: dict, key: str, value, mode="append") -> dict:
     try:
+        if mode in ["overwrite", "ovr", "w"]:
+            raise Exception 
         input[key].append(value)
-    except (KeyError, AttributeError):
+    except: # (KeyError, AttributeError, OverWrite):
         input.update({key: []})
         input[key].append(value)
     return input
